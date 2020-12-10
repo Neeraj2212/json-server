1819c0033c4330c6a8a43c4df99e9d3f2f0be2c3
Neeraj Patil <patilneeraj2212@gmail.com>
2020-12-10 17:55:28 +0530

    Data for React App

---
+++ public/precache-manifest.280574d91e1a843e77e55994885578be.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63554de157647abaed2adef98ba6fa7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca2905fb2643d70f2537",
+    "url": "/static/css/2.e08c6276.chunk.css"
+  },
+  {
+    "revision": "daaf67a5ab1b1af4167a",
+    "url": "/static/css/main.e3ec33a5.chunk.css"
+  },
+  {
+    "revision": "ca2905fb2643d70f2537",
+    "url": "/static/js/2.e8334636.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e8334636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daaf67a5ab1b1af4167a",
+    "url": "/static/js/main.51ba74a3.chunk.js"
+  },
+  {
+    "revision": "33b86f5671812cf49216",
+    "url": "/static/js/runtime-main.24c31a46.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
